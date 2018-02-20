7a6b0d60fc68fe93feb976a1bf204cab488d62a2
Mantas <mantas.muzzz@gmail.com>
2018-02-20 10:26:58 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 4b.
+        //​
+        // Paprašyti vartotojo nurodyti kiek skaičių jis ketina įvesti, tuomet duoti jam įvesti tiek
+        //skaičių, kiek jis pasirinko. Po įvedimo atvaizduoti tik tuos skaičius kurie buvo didesni nei 100.
+
+
+    }
+}
