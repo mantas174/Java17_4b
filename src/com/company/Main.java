1476d5e5f809c082cdb0001181f31fdaad0964fd
Mantas <mantas.muzzz@gmail.com>
2018-02-20 10:28:55 +0200

    padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,25 @@ public class Main {
         // Paprašyti vartotojo nurodyti kiek skaičių jis ketina įvesti, tuomet duoti jam įvesti tiek
         //skaičių, kiek jis pasirinko. Po įvedimo atvaizduoti tik tuos skaičius kurie buvo didesni nei 100.
 
+        Scanner in = new Scanner(System.in);
+        int n;
+        ArrayList<Integer> skaiciuSarasas = new ArrayList<Integer>();
+
+        System.out.print("Iveskite kiek noresite ivesti skaiciu: ");
+        n = in.nextInt();
+
+        for (int i = 0; i < n; i++){
+            System.out.print("Iveskite " + (i+1) + " skaiciu ");
+            skaiciuSarasas.add(in.nextInt());
+        }
+
+
+        System.out.println("Skaiciai didesni nei 100");
+        for (int sk : skaiciuSarasas){
+            if(sk > 100){
+                System.out.println(sk);
+            }
+        }
 
     }
 }
